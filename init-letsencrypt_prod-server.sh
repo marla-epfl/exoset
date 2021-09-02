869dbe6f32eb81e8b3e97bf80ba989c265bba1eb
maria <maria.divincenzo@epfl.ch>
2021-09-02 17:24:07 +0200

    changed domain name for letsencrypt

--- init-letsencrypt_prod-server.sh
+++ init-letsencrypt_prod-server.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(test-exoset.epfl.ch)
+domains=(exoset.epfl.ch)
 rsa_key_size=4096
 data_path="./compose/production/nginx/certbot"
 email="mariacarla.divincenzo@gmail.com" # Adding a valid address is strongly recommended
