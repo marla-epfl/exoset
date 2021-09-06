e851378bb57145fe0a4c643064dea65c4fa3758a
maria <maria.divincenzo@epfl.ch>
2021-09-06 10:53:46 +0200

    reincluded githubadmin app

--- config/urls.py
+++ config/urls.py
@@ -32,8 +32,8 @@ urlpatterns = [
     #path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
     path("resources/", include("exoset.document.urls", namespace="document")),
-    #path("admin_github/", decorator_include([login_required, only_user('github_user')],
-    #     include("exoset.githubadmin.urls", namespace="githubadmin"))),
+    path("admin_github/", decorator_include([login_required, only_user('github_user')],
+         include("exoset.githubadmin.urls", namespace="githubadmin"))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
