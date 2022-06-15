b24f5d8c19df97a0aa99203f21d3f071ec7e8660
maria <maria.divincenzo@epfl.ch>
2022-06-15 19:02:41 +0200

    solved bug pagination when all filters are applied

--- exoset/document/templatetags/group_extra.py
+++ exoset/document/templatetags/group_extra.py
@@ -6,3 +6,11 @@ register = template.Library()
 @register.filter(name='has_group')
 def has_group(user, group_name):
     return user.groups.filter(name=group_name).exists()
+
+
+@register.simple_tag(takes_context=True)
+def query_transform(context, **kwargs):
+    query = context['request'].GET.copy()
+    for k, v in kwargs.items():
+        query[k] = v
+    return query.urlencode()
