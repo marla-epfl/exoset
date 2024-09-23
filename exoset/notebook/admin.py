766c5fe02c8da6adf63c80932ca8a44b79370d92
Maria carla Di Vincenzo <maria.divincenzo@epfl.ch>
2024-09-23 15:41:51 +0200

    added admin for notebook plus modified link for media tag

--- exoset/notebook/admin.py
+++ exoset/notebook/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
-
+from django.contrib.admin import ModelAdmin, register
+from django.utils.translation import gettext as _
 # Register your models here.
+from .models import GitRepository, Notebook
+
+
+@register(GitRepository)
+class GitRepositoryAdmin(ModelAdmin):
+    list_display = ['title', 'course', 'teacher']
+    verbose_name = _('GitRepository')
+    verbose_name_plural = _('GitRepositories')
+
+
+@register(Notebook)
+class NotebookAdmin(ModelAdmin):
+    list_display = ['title', 'git_repository', 'html_view', 'slug']
+    verbose_name = _('Notebook')
+    verbose_name_plural = _('Notebooks')
+
